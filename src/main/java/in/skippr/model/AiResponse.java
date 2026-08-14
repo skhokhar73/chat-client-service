4c9fe3e70c7a3edd8fd83b51ea6301b62a290898
sandeep <Sonu@0403>
2026-08-14 13:50:51 -0400

    add memory chat client functionality with Redis integration

--- src/main/java/in/skippr/model/AiResponse.java
+++ src/main/java/in/skippr/model/AiResponse.java
@@ -4,10 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @Builder
-public class AiResponse {
+public class AiResponse implements Serializable {
     private String responseText;
 
 }
